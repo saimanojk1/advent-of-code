d66f47673d7e5a550e5ae1909b53fa444508ec97
saimanojk1 <saimanojk1@hotmail.com>
2025-12-03 03:29:32 +0100

    Day 2 part 2

--- 2025/pkg/day2.go
+++ 2025/pkg/day2.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -22,7 +23,6 @@ func Day2(inputFilePath string) (int, error) {
 	for idRange := range strings.SplitSeq(line, ",") {
 		startValueString, endValueString, _ := strings.Cut(idRange, "-")
 		slog.Info("", "start Value", startValueString, "end Value", endValueString, "idRange", idRange)
-
 		startValue, err := strconv.Atoi(startValueString)
 		if err != nil {
 			return invalidIdSum, err
@@ -35,54 +35,71 @@ func Day2(inputFilePath string) (int, error) {
 			return invalidIdSum, fmt.Errorf("invalid Ids format start value %d > end value %d", startValue, endValue)
 		}
 
+		mid := 0
 		if len(endValueString)%2 == 0 {
-			mid := len(endValueString) / 2
-			firstHalf := endValueString[:mid]
-			currentDoubleNumber, err := strconv.Atoi(firstHalf + firstHalf)
-			if err != nil {
-				return invalidIdSum, err
-			}
-			for currentDoubleNumber >= startValue {
-				slog.Info("", "currentDouble", currentDoubleNumber)
-				if currentDoubleNumber <= endValue {
-					slog.Info("", "currentDoubleInvalid", currentDoubleNumber)
-					invalidIdSum += currentDoubleNumber
-				}
-				firstHalf = decrementString(firstHalf)
-				currentDoubleNumber, err = strconv.Atoi(firstHalf + firstHalf)
-				if err != nil {
-					return invalidIdSum, err
-				}
-			}
-		} else if len(startValueString)%2 == 0 {
-			mid := len(startValueString) / 2
-			firstHalf := startValueString[:mid]
-			currentDoubleNumber, err := strconv.Atoi(firstHalf + firstHalf)
-			if err != nil {
-				return invalidIdSum, err
-			}
-			for currentDoubleNumber <= endValue {
-				slog.Info("", "currentDouble", currentDoubleNumber)
-				if currentDoubleNumber >= startValue {
-					slog.Info("", "currentDoubleInvalid", currentDoubleNumber)
-					invalidIdSum += currentDoubleNumber
-				}
-				firstHalf = incrementString(firstHalf)
-				currentDoubleNumber, err = strconv.Atoi(firstHalf + firstHalf)
-				if err != nil {
-					return invalidIdSum, err
-				}
-			}
+			mid = len(endValueString) / 2
+		} else {
+			mid = (len(endValueString) + 1) / 2
+		}
+		firstHalf := endValueString[:mid]
+		var repeatedValues []int
+		AddRepeatedValuesInRange(firstHalf, startValue, endValue, &repeatedValues)
+
+		slog.Info("", "repeatedValues", repeatedValues)
+		for _, val := range repeatedValues {
+			invalidIdSum += val
 		}
 	}
 	return invalidIdSum, nil
 }
 
-func incrementString(valString string) string {
-	val, _ := strconv.Atoi(valString)
-	val++
-	return strconv.Itoa(val)
+func AddRepeatedValuesInRange(repeatStringPrefix string, minValue, maxValue int, repeatedValues *[]int) *[]int {
+	//slog.Info("", "repeatStringPrefix", repeatStringPrefix)
+	repeatStringPrefixLen := len(repeatStringPrefix)
+	if repeatStringPrefixLen == 0 {
+		return repeatedValues
+	}
+
+	repeatPrefixVal, _ := strconv.Atoi(repeatStringPrefix)
+	for repeatPrefixVal > 0 {
+		//slog.Info("", "repeatStringPrefix", repeatStringPrefix)
+		minValPrefix, _ := strconv.Atoi(strconv.Itoa(minValue)[:len(repeatStringPrefix)-1])
+		maxValPrefix, _ := strconv.Atoi(strconv.Itoa(maxValue)[:len(repeatStringPrefix)-1])
+		if (repeatPrefixVal < minValPrefix) && (repeatPrefixVal > maxValPrefix) {
+			return AddRepeatedValuesInRange(repeatStringPrefix[:repeatStringPrefixLen-1], minValue, maxValPrefix, repeatedValues)
+		}
+		for lenOfRepeatVals := len(strconv.Itoa(minValue)); lenOfRepeatVals <= len(strconv.Itoa(maxValue)); lenOfRepeatVals++ {
+			repeatedValue, err := GetRepeatedValue(repeatStringPrefix, lenOfRepeatVals)
+			if err != nil {
+				//slog.Debug("", "err", err)
+			} else {
+				if minValue <= repeatedValue && repeatedValue <= maxValue {
+					//slog.Info("", "repeatedValue", repeatedValue)
+					if !slices.Contains(*repeatedValues, repeatedValue) {
+						*repeatedValues = append(*repeatedValues, repeatedValue)
+					}
+				}
+			}
+		}
+		repeatStringPrefix = decrementString(repeatStringPrefix)
+		repeatPrefixVal, _ = strconv.Atoi(repeatStringPrefix)
+	}
+	return repeatedValues
 }
+
+func GetRepeatedValue(repeatString string, maxSize int) (int, error) {
+	repeatStringSize := len(repeatString)
+	if maxSize%repeatStringSize == 0 && maxSize/repeatStringSize > 1 {
+		repeatedString := strings.Repeat(repeatString, maxSize/repeatStringSize)
+		repeatedVal, err := strconv.Atoi(repeatedString)
+		if err != nil {
+			return 0, fmt.Errorf("not possible to repeat %v", err)
+		}
+		return repeatedVal, nil
+	}
+	return 0, fmt.Errorf("not possible to repeat")
+}
+
 func decrementString(valString string) string {
 	val, _ := strconv.Atoi(valString)
 	val--
--- 2025/pkg/day2_test.go
+++ 2025/pkg/day2_test.go
@@ -11,5 +11,27 @@ func TestDay2(t *testing.T) {
 	inputFilePath := filepath.Join(INPUTFILES_DIR, "day2-test-input.txt")
 	invalidSum, err := Day2(inputFilePath)
 	require.NoError(t, err)
-	require.Equal(t, 1227775554, invalidSum)
+	require.Equal(t, 4174379265, invalidSum)
+}
+
+func TestGetRepeatedString(t *testing.T) {
+	repeatedString, err := GetRepeatedValue("1", 5)
+	require.NoError(t, err)
+	require.Equal(t, 11111, repeatedString)
+}
+
+func TestAddRepeatedValuesInRange(t *testing.T) {
+	var repeatedValues []int
+	AddRepeatedValuesInRange("1", 5, 12, &repeatedValues)
+	require.Contains(t, repeatedValues, 11)
+	repeatedValues = repeatedValues[:0]
+	AddRepeatedValuesInRange("82482", 824824821, 824824827, &repeatedValues)
+	require.Contains(t, repeatedValues, 824824824)
+}
+
+func TestAddRepeatedValuesInRange2(t *testing.T) {
+	var repeatedValues []int
+	AddRepeatedValuesInRange("10", 998, 1012, &repeatedValues)
+	require.Contains(t, repeatedValues, 999)
+	require.Contains(t, repeatedValues, 1010)
 }
